1fcf9bd729d487fd1da6e7a5e74991c5e401ce69
kiran <gandikotakiran99@gmail.com>
2020-05-27 20:15:40 +0530

    rendering list of songs which are hardcoded

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const SongList = () => {
+    return ( 
+        <div className="song-list">
+            <ul>
+                <li>Bring Back Memories</li>
+                <li>Shadow to the life</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
