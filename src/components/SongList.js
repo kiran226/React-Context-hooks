dfd79196af1a70fa72e60d0e71221a04189f0f7a
kiran <gandikotakiran99@gmail.com>
2020-05-29 11:54:14 +0530

    add a song with input field

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,24 +1,26 @@
 import React,{useState} from 'react';
-import uuid from 'uuid/dist/v1';
-const SongList = () => {
-   const[songs,setSongs]= useState([
-        {title:"Bring Back Memories", id:1},
-        {title:"Shape of You", id:2},
-        {title:"Shadow to the life",id:3}
-    ]);
-    const addSong=()=>{
-        setSongs([...songs,{title:'Taylor swift', id:uuid()}])
-    }
-    return ( 
+import uuid from 'uuid/dist/v1'
+import NewSongForm from './NewSongForm';
+const SongList=()=>{
+    const [songs,setSongs]=useState([
+            {title: 'Bring back memories',id: 1},
+            {title: 'Shadow to the life', id:2},
+            {title: 'Shape of You', id: 3}
+        ]);
+    const addSong=(title)=>{
+            setSongs([...songs,{title, id:uuid()}])
+        }
+    return(
         <div className="song-list">
             <ul>
                 {songs.map((song)=>{
                     return (<li key={song.id}>{song.title}</li>)
                 })}
             </ul>
-            <button onClick={addSong}>Add song</button>
+            <NewSongForm addSong={addSong}/>
+
+           
         </div>
-     );
+    )
 }
- 
 export default SongList;
