e0494f95958ae332835829fd24f023dde1e8ed5c
kiran <gandikotakiran99@gmail.com>
2020-06-03 10:45:04 +0530

    useEffect hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import uuid from 'uuid/dist/v1'
 import NewSongForm from './NewSongForm';
 const SongList=()=>{
@@ -7,9 +7,17 @@ const SongList=()=>{
             {title: 'Shadow to the life', id:2},
             {title: 'Shape of You', id: 3}
         ]);
+    const [age,setAge]=useState(20);    
     const addSong=(title)=>{
             setSongs([...songs,{title, id:uuid()}])
         }
+    useEffect(()=>{
+        console.log("useEffect ran",songs);
+    },[songs]);
+    useEffect(()=>{
+        console.log("useEffect ran",age);
+    },[age]);
+    
     return(
         <div className="song-list">
             <ul>
@@ -18,8 +26,7 @@ const SongList=()=>{
                 })}
             </ul>
             <NewSongForm addSong={addSong}/>
-
-           
+            <button onClick={()=>setAge(age+1)}>Add 1 to {age}</button>
         </div>
     )
 }
